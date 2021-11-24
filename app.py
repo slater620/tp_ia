9e0bdc3aef7d0fec5476eb616d0f1a5b914ac4cd
ruben slater <chendjouruben@gmail.com>
2021-11-24 16:07:20 +0100

    mon tp de IA

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
+app = Flask(__name__)
+
+#chargement des modèles œ&
+file1 = open("count_vectorizer_model.sav", 'rb')
+vectorizer = pickle.load(file1)
+
+file2 = open("logistic_regression_model.sav", 'rb')
+lr = pickle.load(file2)
+
+@app.route('/')
+def man():
+    return render_template('index.html')
+
+@app.route("/api/spamdetector", methods=["POST","GET"])
+def detector():
+    mail = request.form["mail"]
+    mail_2 = vectorizer.transform([mail]).toarray()
+    p = lr.predict_proba(mail_2.reshape(1, -1))[0]
+    print("Ce mail est un ham à ",p[1],"%")
+    print("Ce mail est un spam à",p[0],"%")
+    message = "ce mail est un spam à " + str(p[0]) + " %."
+    return render_template('index.html', message=message)
+
+
+
+if __name__ == "__main__":
+    app.run(port=8001, debug=True, host="127.0.0.1")
